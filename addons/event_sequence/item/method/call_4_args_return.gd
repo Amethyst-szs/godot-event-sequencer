b2abed1877e4de7cab1cda8fc6a4dd71f8b0ebc0
Amethyst-szs <Amethyst-UwU@protonmail.com>
2023-12-11 12:32:57 -0800

    Added call with args and get return

--- addons/event_sequence/item/method/call_4_args_return.gd
+++ addons/event_sequence/item/method/call_4_args_return.gd
@@ -3,13 +3,11 @@ extends EventItemBase
 
 var return_data
 
-#region Config
-
 func get_name() -> String:
-	return "Call Method (Get Return)"
+	return "Call Method (Args and Get Return)"
 
 func get_description() -> String:
-	return "Call method on object/node or list of objects/nodes and save the return (no arguments)
+	return "Call method on object/node or list of objects/nodes with arguments save the return
 		If multiple objects are passed in, new variable will be array of returns from each obj."
 
 func get_editor_tab() -> EventConst.EditorDialogTab:
@@ -47,15 +45,20 @@ func get_userdata_keys() -> Array[Dictionary]:
 			"type": TYPE_STRING,
 			"require": true,
 		},
+		{
+			"name": "args",
+			"desc": "Variable name with Argument",
+			"type": TYPE_ARRAY,
+			"type_array": TYPE_STRING,
+			"require": false,
+		},
 	]
 
 func get_color() -> Color:
 	return Color.LIME_GREEN
 
 func get_icon_path() -> String:
-	return "res://addons/event_sequence/icon/EventItem-MethodCallRet.svg"
-
-#endregion
+	return "res://addons/event_sequence/icon/EventItem-MethodCallArgsReturn.svg"
 
 func run(event_node: EventNode) -> EventConst.ItemResponseType:
 	if not is_valid_event_variable(event_node, true):
@@ -70,15 +73,25 @@ func run(event_node: EventNode) -> EventConst.ItemResponseType:
 	
 	var data = event_node.var_database[userdata["object"]]
 	
+	# Create list of argument data
+	var arguments: Array = []
+	for item in userdata["args"]:
+		if not event_node.var_database.has(item):
+			warn("Variable \"%s\" doesn't exist, cannot be used as argument, skipping function call"
+				% [item])
+			return EventConst.ItemResponseType.OK
+		
+		arguments.push_back(event_node.var_database[item])
+	
 	# Call method on object or array of object
 	match(typeof(data)):
 		TYPE_OBJECT:
-			call_on_object(data as Object, false)
+			call_on_object(data as Object, arguments, false)
 		TYPE_ARRAY:
 			return_data = Array()
 			for item in data:
 				if typeof(item) == TYPE_OBJECT:
-					call_on_object(item as Object, true)
+					call_on_object(item as Object, arguments, true)
 		_:
 			warn("Data in variable \"%s\" isn't an object or array" % [userdata["object"]])
 			return EventConst.ItemResponseType.OK
@@ -88,13 +101,13 @@ func run(event_node: EventNode) -> EventConst.ItemResponseType:
 	
 	return EventConst.ItemResponseType.OK
 
-func call_on_object(object: Object, is_part_of_array: bool):
+func call_on_object(object: Object, arguments: Array, is_part_of_array: bool):
 	if not object.has_method(userdata["method"]):
 		warn("Object in variable \"%s\" doesn't have method \"%s\"" % [userdata["object"], userdata["method"]])
 		return
 	
 	var call := Callable(object, userdata["method"])
 	if not is_part_of_array:
-		return_data = call.call()
+		return_data = call.callv(arguments)
 	else:
-		return_data.push_back(call.call())
+		return_data.push_back(call.callv(arguments))
