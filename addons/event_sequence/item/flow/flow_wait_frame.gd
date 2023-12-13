6a8f260bcf4d5675ed5af8c589843ebdd8daf2ce
Amethyst-szs <Amethyst-UwU@protonmail.com>
2023-12-13 13:57:55 -0800

    Added "Wait for Single Frame" item

---
+++ addons/event_sequence/item/flow/flow_wait_frame.gd
@@ -0,0 +1,50 @@
+@tool
+extends EventItemFlowBase
+
+signal timer_complete
+
+#region Config
+
+func get_name() -> String:
+	return "Wait for Single Frame"
+
+func get_description() -> String:
+	return "Wait at this point until the next process frame. Useful for preventing large
+		event sequences with no other waiting from causing lag on slower machines."
+
+func is_allow_in_editor() -> bool:
+	return true
+
+func get_first_column_config() -> Dictionary:
+	return {
+		"name": "None",
+		"editable": false,
+		"cell_mode": TreeItem.CELL_MODE_STRING
+	}
+
+func get_second_column_config() -> Dictionary:
+	return {
+		"name": "None",
+		"editable": false,
+		"cell_mode": TreeItem.CELL_MODE_STRING
+	}
+
+func get_icon_path() -> String:
+	return "res://addons/event_sequence/icon/EventItem-FlowWaitFrame.svg"
+
+#endregion
+
+func run(event_node: EventNode) -> EventConst.ItemResponseType:
+	var timer := Timer.new()
+	event_node.add_child(timer)
+	timer.wait_time = 0.000001
+	
+	timer.timeout.connect(_timer_complete_trigger)
+	timer.start()
+	await timer_complete
+	
+	timer.queue_free()
+	return EventConst.ItemResponseType.OK
+
+func _timer_complete_trigger() -> void:
+	timer_complete.emit()
