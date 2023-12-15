d009d260177483f9c85c5668fe95bf383202a983
Amethyst-szs <Amethyst-UwU@protonmail.com>
2023-12-15 11:59:15 -0800

    Automatically open EventEditor on EventNode click

--- addons/event_sequence/plugin.gd
+++ addons/event_sequence/plugin.gd
@@ -14,10 +14,29 @@ func _enter_tree():
 	editor_view.editor_plugin = self
 	get_editor_interface().get_editor_main_screen().add_child(editor_view)
 	_make_visible(false)
+	
+	# Connect the editor's selection change signal to func
+	var selection := get_editor_interface().get_selection()
+	selection.selection_changed.connect(_selection_changed)
 
 func _exit_tree():
 	if is_instance_valid(editor_view):
 		editor_view.queue_free()
+	
+	# Remove selection change signal
+	var selection := get_editor_interface().get_selection()
+	if selection.selection_changed.is_connected(_selection_changed):
+		selection.selection_changed.disconnect(_selection_changed)
+
+func _selection_changed():
+	# Check if any node is selected
+	var selection := get_editor_interface().get_selection()
+	if selection.get_selected_nodes().is_empty():
+		return
+	
+	# If this node is an EventNode, switch editor screen
+	if selection.get_selected_nodes()[0] is EventNode:
+		get_editor_interface().set_main_screen_editor("Event")
 
 func _has_main_screen():
 	return true
